eb6aac5a1dcbb0663b99c63fef8c36cfc240f223
JIE ZHANG <29704759+gongwaner@users.noreply.github.com>
2024-10-17 21:50:06 +0800

    update io util functions
    1.add imagedata io functions
    2.use const std::string& instead of const char*

--- IO/IOUtil.cpp
+++ IO/IOUtil.cpp
@@ -10,9 +10,12 @@
 #include <vtkPolyDataReader.h>
 
 #include <vtkImageData.h>
+#include <vtkJPEGReader.h>
+#include <vtkPNGReader.h>
 #include <vtkDICOMImageReader.h>
 #include <vtkImageCast.h>
 #include <vtkPNGWriter.h>
+#include <vtkJPEGWriter.h>
 
 #include <iostream>
 #include <filesystem>
@@ -23,13 +26,13 @@
 
 namespace IOUtil
 {
-    std::string GetFileExtension(const char* fileDir)
+    std::string GetFileExtension(const std::string& fileDir)
     {
         const auto path = std::filesystem::path(fileDir);
         return path.extension().string();
     }
 
-    bool PathExist(const char* fileDir)
+    bool PathExist(const std::string& fileDir)
     {
         const auto path = std::filesystem::path(fileDir);
         if(!std::filesystem::exists(path))
@@ -41,7 +44,7 @@ namespace IOUtil
         return true;
     }
 
-    bool IsValidDirectory(const char* fileDir)
+    bool IsValidDirectory(const std::string& fileDir)
     {
         const auto path = std::filesystem::path(fileDir);
         const auto parentDir = path.parent_path();
@@ -54,7 +57,7 @@ namespace IOUtil
         return true;
     }
 
-    void CreateParentDirectory(const char* fileDir)
+    void CreateParentDirectory(const std::string& fileDir)
     {
         const auto path = std::filesystem::path(fileDir);
         const auto parentDir = path.parent_path();
@@ -63,7 +66,7 @@ namespace IOUtil
         std::filesystem::create_directories(parentDir);
     }
 
-    vtkSmartPointer<vtkPolyData> ReadMesh(const char* fileDir)
+    vtkSmartPointer<vtkPolyData> ReadMesh(const std::string& fileDir)
     {
         std::cout << "Reading " << fileDir << std::endl;
 
@@ -71,32 +74,34 @@ namespace IOUtil
             throw std::runtime_error(std::string(__FUNCTION__) + "() ERROR: Path does not exist!");
 
         const auto extension = GetFileExtension(fileDir);
+        const auto fileName = fileDir.c_str();
         vtkSmartPointer<vtkPolyData> polyData;
+
         if(extension == PlyExtension)
         {
             auto reader = vtkSmartPointer<vtkPLYReader>::New();
-            reader->SetFileName(fileDir);
+            reader->SetFileName(fileName);
             reader->Update();
             polyData = reader->GetOutput();
         }
         else if(extension == ObjExtension)
         {
             auto reader = vtkSmartPointer<vtkOBJReader>::New();
-            reader->SetFileName(fileDir);
+            reader->SetFileName(fileName);
             reader->Update();
             polyData = reader->GetOutput();
         }
         else if(extension == StlExtension)
         {
             auto reader = vtkSmartPointer<vtkSTLReader>::New();
-            reader->SetFileName(fileDir);
+            reader->SetFileName(fileName);
             reader->Update();
             polyData = reader->GetOutput();
         }
         else if(extension == VtkExtension)
         {
             auto reader = vtkSmartPointer<vtkPolyDataReader>::New();
-            reader->SetFileName(fileDir);
+            reader->SetFileName(fileName);
             reader->Update();
             polyData = reader->GetOutput();
         }
@@ -110,30 +115,32 @@ namespace IOUtil
         return polyData;
     }
 
-    void WriteMesh(const char* fileDir, vtkPolyData* polyData)
+    void WriteMesh(const std::string& fileDir, vtkPolyData* polyData)
     {
         if(!IsValidDirectory(fileDir))
             CreateParentDirectory(fileDir);
 
         const auto extension = GetFileExtension(fileDir);
+        const auto fileName = fileDir.c_str();
+
         if(extension == PlyExtension)
         {
             auto writer = vtkSmartPointer<vtkPLYWriter>::New();
-            writer->SetFileName(fileDir);
+            writer->SetFileName(fileName);
             writer->SetInputData(polyData);
             writer->Write();
         }
         else if(extension == ObjExtension)
         {
             auto writer = vtkSmartPointer<vtkOBJWriter>::New();
-            writer->SetFileName(fileDir);
+            writer->SetFileName(fileName);
             writer->SetInputData(polyData);
             writer->Write();
         }
         else if(extension == StlExtension)
         {
             auto writer = vtkSmartPointer<vtkSTLWriter>::New();
-            writer->SetFileName(fileDir);
+            writer->SetFileName(fileName);
             writer->SetInputData(polyData);
             writer->Write();
         }
@@ -146,7 +153,7 @@ namespace IOUtil
         std::cout << "Poly data is written to " << fileDir << std::endl;
     }
 
-    void WriteColorMesh(const char* fileDir, vtkPolyData* polyData)
+    void WriteColorMesh(const std::string& fileDir, vtkPolyData* polyData)
     {
         if(GetFileExtension(fileDir) != PlyExtension)
         {
@@ -164,7 +171,7 @@ namespace IOUtil
             CreateParentDirectory(fileDir);
 
         auto plyWriter = vtkSmartPointer<vtkPLYWriter>::New();
-        plyWriter->SetFileName(fileDir);
+        plyWriter->SetFileName(fileDir.c_str());
         plyWriter->SetInputData(polyData);
         plyWriter->SetArrayName(Color::ColorArrayName.c_str()); //set colors
         plyWriter->Write();
@@ -172,7 +179,7 @@ namespace IOUtil
         std::cout << "Saved colored polydata to " << fileDir << std::endl;
     }
 
-    void WriteFeatureVectorToFile(const char* dir, const std::vector<double>& dataVector, const bool append)
+    void WriteFeatureVectorToFile(const std::string& dir, const std::vector<double>& dataVector, const bool append)
     {
         if(!IsValidDirectory(dir))
             CreateParentDirectory(dir);
@@ -191,24 +198,59 @@ namespace IOUtil
         std::cout << "Saved feature file to " << dir << std::endl;
     }
 
-    vtkSmartPointer<vtkImageData> ReadImageData(const char* folder)
+    vtkSmartPointer<vtkImageData> ReadImageData(const std::string& fileDir)
     {
-        if(!PathExist(folder))
+        std::cout << "Reading " << fileDir << std::endl;
+
+        if(!PathExist(fileDir))
+            throw std::runtime_error(std::string(__FUNCTION__) + "() ERROR: Path does not exist!");
+
+        const auto extension = GetFileExtension(fileDir);
+        const auto fileName = fileDir.c_str();
+        vtkSmartPointer<vtkImageData> imageData;
+
+        if(extension == JpegExtension)
+        {
+            auto reader = vtkSmartPointer<vtkJPEGReader>::New();
+            reader->SetFileName(fileName);
+            reader->Update();
+
+            imageData = reader->GetOutput();
+        }
+        else if(extension == PngExtension)
+        {
+            auto reader = vtkSmartPointer<vtkPNGReader>::New();
+            reader->SetFileName(fileName);
+            reader->Update();
+
+            imageData = reader->GetOutput();
+        }
+        else
+        {
+            throw std::runtime_error(std::string(__FUNCTION__) + "() ERROR: unsupported file extension!");
+        }
+
+        return imageData;
+    }
+
+    vtkSmartPointer<vtkImageData> ReadDicomImageData(const std::string& dicomFolder)
+    {
+        if(!PathExist(dicomFolder))
         {
             throw std::runtime_error(std::string(__FUNCTION__) + "(). Folder does not exist!");
         }
 
-        const auto path = std::filesystem::path(folder);
+        const auto path = std::filesystem::path(dicomFolder);
         std::cout << "reading " << path << std::endl;
 
         auto reader = vtkSmartPointer<vtkDICOMImageReader>::New();
-        reader->SetDirectoryName(folder);
+        reader->SetDirectoryName(dicomFolder.c_str());
         reader->Update();
 
         return reader->GetOutput();
     }
 
-    void WritePng(const char* fileDir, vtkImageData* imageData, const bool isUnsignedShort)
+    void WritePng(const std::string& fileDir, vtkImageData* imageData, const bool isUnsignedShort)
     {
         if(!IsValidDirectory(fileDir))
         {
@@ -227,8 +269,25 @@ namespace IOUtil
         auto output = castFilter->GetOutput();
 
         auto writer = vtkSmartPointer<vtkPNGWriter>::New();
-        writer->SetFileName(fileDir);
+        writer->SetFileName(fileDir.c_str());
         writer->SetInputData(output);
+        writer->Write();
+    }
+
+    void WriteJpeg(const std::string& fileDir, vtkImageData* imageData)
+    {
+        if(!IsValidDirectory(fileDir))
+        {
+            std::cout << "Creating Directory..." << std::endl;
+
+            const auto path = std::filesystem::path(fileDir);
+            const auto parentDir = path.parent_path();
+            std::filesystem::create_directories(parentDir);
+        }
+
+        vtkNew<vtkJPEGWriter> writer;
+        writer->SetFileName(fileDir.c_str());
+        writer->SetInputData(imageData);
         writer->Write();
     }
 }
--- IO/IOUtil.h
+++ IO/IOUtil.h
@@ -1,8 +1,9 @@
 #pragma once
 
-#include <vtkVector.h>
+#include <vtkSmartPointer.h>
 
 #include <vector>
+#include <string>
 
 
 class vtkPolyData;
@@ -10,19 +11,25 @@ class vtkImageData;
 
 namespace IOUtil
 {
-    //extensions
+    //extensions for polydata
     const std::string PlyExtension = ".ply";
     const std::string ObjExtension = ".obj";
     const std::string StlExtension = ".stl";
     const std::string VtkExtension = ".vtk";
 
+    //extension for imagedata
+    const std::string JpegExtension = ".jpg";
+    const std::string PngExtension = ".png";
+
     //poly data
-    vtkSmartPointer<vtkPolyData> ReadMesh(const char* fileDir);
-    void WriteMesh(const char* fileDir, vtkPolyData* polyData);
-    void WriteColorMesh(const char* fileDir, vtkPolyData* polyData);
-    void WriteFeatureVectorToFile(const char* dir, const std::vector<double>& dataVector, bool append = false);
+    vtkSmartPointer<vtkPolyData> ReadMesh(const std::string& fileDir);
+    void WriteMesh(const std::string& fileDir, vtkPolyData* polyData);
+    void WriteColorMesh(const std::string& fileDir, vtkPolyData* polyData);
+    void WriteFeatureVectorToFile(const std::string& dir, const std::vector<double>& dataVector, bool append = false);
 
     //image data
-    vtkSmartPointer<vtkImageData> ReadImageData(const char* folder);
-    void WritePng(const char* fileDir, vtkImageData* imageData, bool isUnsignedShort = true);
+    vtkSmartPointer<vtkImageData> ReadImageData(const std::string& fileDir);
+    vtkSmartPointer<vtkImageData> ReadDicomImageData(const std::string& dicomFolder);
+    void WritePng(const std::string& fileDir, vtkImageData* imageData, bool isUnsignedShort = true);
+    void WriteJpeg(const std::string& fileDir, vtkImageData* imageData);
 }
